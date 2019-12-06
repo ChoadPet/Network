c5e4ac16e653e7a924191e1cab2daca72419a106
twix <twix@inernetbs.net>
2019-12-06 23:02:57 +0200

    Initial commit

---
+++ NetworkManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkManager
+//
+//  Created by Vetaliy Poltavets on 12/6/19.
+//  Copyright © 2019 vpoltave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
